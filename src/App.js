72e2c9e822ab7e601674a77d4869a3cee1f3e85f
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-06 01:23:28 -0600

    Post added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 import {config} from './config';
+import { wait } from "@testing-library/dom";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const fireBaseServer  = `${config.db}movies.json`
 
   //useEffect is only called on the update of the second argument, since
   //there is an empty array that never changes, it will handle the first
@@ -18,22 +21,26 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(fireBaseServer);
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+
+        })
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -46,8 +53,17 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  //Add movie to firebase server
+  async function addMovieHandler(movie) {
+    const response = await fetch(fireBaseServer, {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data)
   }
 
   let content = <p>Found no movies.</p>;
